785cdd95cde9b30662d982c76004d832b77d2763
Colin Vaufrey <colinvaufrey@gmail.com>
2023-11-21 19:11:43 +0100

    Remove isBox variable from NetworkTransmitter Update HouseMiniGame scene

--- Assets/Scripts/Tasks/Houses/NetworkTransmitter.cs
+++ Assets/Scripts/Tasks/Houses/NetworkTransmitter.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class NetworkTransmitter : MonoBehaviour
 {
-    public bool isBox = false;
     private void Awake()
     {
         GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
