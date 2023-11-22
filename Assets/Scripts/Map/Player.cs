e15701223f546c5b5977b3282b4426b742a1942d
Poulpefaible <60007891+Poulpefaible@users.noreply.github.com>
2023-11-23 00:09:08 +0100

    animated movement

--- Assets/Scripts/Map/Player.cs
+++ Assets/Scripts/Map/Player.cs
@@ -1,13 +1,28 @@
+using Pathfinding;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    public Animator animator;
 
-    void FixedUpdate()
+    [SerializeField] private float speed = 5f;
+    public AIPath aiPath;
+
+    private void Update()
     {
-        Vector3 input = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        input.Normalize();
-        transform.position += speed * Time.deltaTime * input;
+        //Debug.Log(aiPath.desiredVelocity.magnitude);
+        animator.SetFloat("speed", Mathf.Abs(speed * Time.deltaTime * aiPath.desiredVelocity.magnitude));
+
+        if (Mathf.Abs(aiPath.desiredVelocity.x) > Mathf.Abs(aiPath.desiredVelocity.y))
+        {
+            if (aiPath.desiredVelocity.x > 0) { animator.SetTrigger("right"); }
+            else { animator.SetTrigger("left"); }
+        }
+        else
+        {
+            if (aiPath.desiredVelocity.y > 0) { animator.SetTrigger("back"); }
+            else { animator.SetTrigger("front"); }
+        }
     }
 }
